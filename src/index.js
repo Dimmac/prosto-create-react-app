5c037dda439ca9fa47e9a007d08bcaae8ffbe68e
Dimmac <dmytro.lojivsky@gmail.com>
2022-04-13 14:28:39 +0300

    server

--- src/index.js
+++ src/index.js
@@ -9,3 +9,12 @@ root.render(
     <App />
   </React.StrictMode>
 );
+const dataId = Date.now();
+console.log(dataId + 60 * 60 * 24 * 1.1);
+console.log(dataId + 60 * 60 * 24 * 1.2);
+console.log(dataId + 60 * 60 * 24 * 1.3);
+console.log(dataId + 60 * 60 * 24 * 1.4);
+console.log(dataId + 60 * 60 * 24 * 1.5);
+console.log(dataId + 60 * 60 * 24 * 1.6);
+console.log(dataId + 60 * 60 * 24 * 1.7);
+console.log(dataId + 60 * 60 * 24 * 1.8);
